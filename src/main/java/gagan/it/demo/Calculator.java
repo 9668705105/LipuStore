2e0e2e08c2bf321252da85f9f71c9ae12269f659
9668705105 <gmuduli145@gmail.com>
2021-07-23 07:41:45 +0530

    for correction purpose

--- src/main/java/gagan/it/demo/Calculator.java
+++ src/main/java/gagan/it/demo/Calculator.java
@@ -3,6 +3,7 @@ package gagan.it.demo;
 public class Calculator {
 
 	public static void main(String[] args) {
+                int p=30;
 		int i=20;
 		int j=30;
 		System.out.println("Result is"+(i+j));
